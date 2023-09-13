6e4d26e03412777c8768c33a0ebb360f53f78a5c
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2023-09-13 09:12:33 +0530

    responsiveness added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -29,167 +29,31 @@ function Navbar() {
 
   return (
     <>
-      <div className="flex flex-row justify-between text-xs md:text-lg bg-theme-dark text-white  w-full p-2 fixed top-0  h-14 z-10">
-        <div
-          className="mx-2 cursor-pointer"
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          <img className="w-20 h-10" src={logo} alt="logo" />
+      <nav className="flex items-center justify-between flex-wrap px-2 py-2 bg-theme-dark text-white fixed top-0 left-0 z-10 w-screen">
+        <div className="flex items-center flex-shrink-0 text-white mr-6  w-1/4 lg:w-fit">
+          <Link to="/">
+            <img src={logo} className="w-100 h-10 mr-2" alt="Logo" />
+          </Link>
         </div>
-        <div
-          className={`flex justify-between gap-5 lg:gap-10  w-fit items-center text-xs md:text-base lg:text-lg `}
-        >
-          <Link
-            to="/"
-            className={
-              ` hover:text-theme-yellow-dark  cursor-pointer ` +
-              (currentUrl == "" ? "text-theme-yellow-dark" : "")
-            }
-          >
-            New
-          </Link>
-          <Link
-            to="/all"
-            className={
-              ` hover:text-theme-yellow-dark  cursor-pointer ` +
-              (currentUrl == "all" ? "text-theme-yellow-dark" : "")
-            }
-          >
-            All
-          </Link>
-          <Link
-            to="/resolved"
-            className={
-              ` hover:text-theme-yellow-dark  cursor-pointer ` +
-              (currentUrl == "resolved" ? "text-theme-yellow-dark" : "")
-            }
-          >
-            Resolved
-          </Link>
-          <Link
-            to="/unresolved"
-            className={
-              ` hover:text-theme-yellow-dark  cursor-pointer ` +
-              (currentUrl == "unresolved" ? "text-theme-yellow-dark" : "")
-            }
-          >
-            Unresolved
-          </Link>
-          {userInfo?.isAdmin && (
-            <>
-              <Link
-                to="/stats"
-                className={
-                  ` hover:text-theme-yellow-dark  cursor-pointer ` +
-                  (currentUrl == "stats" ? "text-theme-yellow-dark" : "")
-                }
-              >
-                Stats
-              </Link>
-            </>
-          )}
-          {!userInfo?.isAdmin && (
-            <>
-              <Link
-                to="/profile"
-                className={
-                  ` hover:text-theme-yellow-dark  cursor-pointer ` +
-                  (currentUrl == "profile" ? "text-theme-yellow-dark" : "")
-                }
-              >
-                Profile
-              </Link>
-              <Link
-                to="/history"
-                className={
-                  ` hover:text-theme-yellow-dark  cursor-pointer ` +
-                  (currentUrl == "history" ? "text-theme-yellow-dark" : "")
-                }
-              >
-                History
-              </Link>
-            </>
-          )}
-          {userInfo?.isAdmin && (
-            <>
-              <Link
-                to="/dashboard"
-                className={
-                  ` hover:text-theme-yellow-dark  cursor-pointer ` +
-                  (currentUrl == "dashboard" ? "text-theme-yellow-dark" : "")
-                }
-              >
-                Dashboard
-              </Link>
-            </>
-          )}
-          <Link
-            to="/topperformers"
-            className={
-              ` hover:text-theme-yellow-dark  cursor-pointer whitespace-nowrap ` +
-              (currentUrl == "topperformers" ? "text-theme-yellow-dark" : "")
-            }
-          >
-            Top Performers
-          </Link>
-          <Link
-            to="/announcements"
-            className={
-              ` hover:text-theme-yellow-dark  cursor-pointer whitespace-nowrap ` +
-              (currentUrl == "announcements" ? "text-theme-yellow-dark" : "")
-            }
-          >
-            Announcements
-          </Link>
 
-          {/* {userInfo?.isAdmin && (
-          <Link
-            to="/adduser"
-            className="text-sm md:text-lg  hover:text-theme-yellow-dark  cursor-pointer"
-          >
-            Add User
-          </Link>
-        )} */}
-          <Link
-            to="/info"
-            className={
-              ` hover:text-theme-yellow-dark  cursor-pointer ` +
-              (currentUrl == "info" ? "text-theme-yellow-dark" : "")
-            }
-          >
-            <i className="fa-solid fa-circle-info"></i>
-          </Link>
+        <div className="block lg:hidden mx-auto w-fit capitalize text-lg">
+          {currentUrl === ""
+            ? "New"
+            : currentUrl === "topperformers"
+            ? "Top Performers"
+            : currentUrl}
         </div>
-        {userInfo ? (
-          <a
-            className="my-auto mx-2 hover:bg-red-600  hover:text-white text-red-600 active:text-red-600 px-2 py-1 rounded-lg  cursor-pointer"
-            onClick={logOut}
-          >
-            Logout
-          </a>
-        ) : (
-          <Link
-            to="/login"
-            className="my-auto mx-2  text-theme-yellow-dark active:text-theme-yellow-dark px-2 py-1 rounded-lg  cursor-pointer"
-            // onClick={() => setAuth(true)}
-          >
-            Login
-          </Link>
-        )}
-      </div>
-      {/* <nav className="flex items-center justify-between flex-wrap p-6">
-        <div className="flex items-center flex-shrink-0 text-white mr-6 lg:mr-72">
-          <img src={logo} className="w-100 h-10 mr-2" alt="Logo" />
-        </div>
-        <div className="block lg:hidden">
+
+        <div
+          className=" lg:hidden w-1/4 lg:w-fit flex justify-end pr-1 sm:pr-5
+"
+        >
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+            className="flex items-center px-3  py-2 rounded text-black-500 hover:text-black-400 "
           >
             <svg
-              className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
+              className={`fill-current h-3 w-3  ${isOpen ? "hidden" : "block"}`}
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -205,48 +69,152 @@ function Navbar() {
           </button>
         </div>
         <div
-          className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${
+          className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto py-2 lg:py-0 ${
             isOpen ? "block" : "hidden"
           }`}
         >
-          <div className="text-sm lg:flex-grow">
-            <a
-              href="#"
-              className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4"
+          <div className="text-sm lg:flex-grow ">
+            <Link
+              to="/"
+              className={
+                `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                (currentUrl == "" ? "text-theme-yellow-dark" : "")
+              }
             >
-              First Link
-            </a>
-            <a
-              href="#"
-              className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4"
+              New
+            </Link>
+            <Link
+              to="/all"
+              className={
+                `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                (currentUrl == "all" ? "text-theme-yellow-dark" : "")
+              }
             >
-              Second Link
-            </a>
-            <a
-              href="#"
-              className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4"
+              All
+            </Link>
+            <Link
+              to="/resolved"
+              className={
+                `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                (currentUrl == "resolved" ? "text-theme-yellow-dark" : "")
+              }
             >
-              Third Link
-            </a>
-            <div class="dropdown">
-              <button class="dropbtn">
-                Dropdown
-                <i class="fa fa-caret-down"></i>
-              </button>
-              <div class="dropdown-content">
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-              </div>
-            </div>
+              Resolved
+            </Link>
+            <Link
+              to="/unresolved"
+              className={
+                `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                (currentUrl == "unresolved" ? "text-theme-yellow-dark" : "")
+              }
+            >
+              Unresolved
+            </Link>
+            {userInfo?.isAdmin && (
+              <>
+                <Link
+                  to="/stats"
+                  className={
+                    `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                    (currentUrl == "stats" ? "text-theme-yellow-dark" : "")
+                  }
+                >
+                  Stats
+                </Link>
+              </>
+            )}
+            {!userInfo?.isAdmin && (
+              <>
+                <Link
+                  to="/profile"
+                  className={
+                    `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                    (currentUrl == "profile" ? "text-theme-yellow-dark" : "")
+                  }
+                >
+                  Profile
+                </Link>
+                <Link
+                  to="/history"
+                  className={
+                    `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                    (currentUrl == "history" ? "text-theme-yellow-dark" : "")
+                  }
+                >
+                  History
+                </Link>
+              </>
+            )}
+            {userInfo?.isAdmin && (
+              <>
+                <Link
+                  to="/dashboard"
+                  className={
+                    `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                    (currentUrl == "dashboard" ? "text-theme-yellow-dark" : "")
+                  }
+                >
+                  Dashboard
+                </Link>
+              </>
+            )}
+            <Link
+              to="/topperformers"
+              className={
+                `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                (currentUrl == "topperformers" ? "text-theme-yellow-dark" : "")
+              }
+            >
+              Top Performers
+            </Link>
+            <Link
+              to="/announcements"
+              className={
+                `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                (currentUrl == "announcements" ? "text-theme-yellow-dark" : "")
+              }
+            >
+              Announcements
+            </Link>
+
+            <Link
+              to="/info"
+              className={
+                `px-2 block my-3 lg:my-0 text-lg lg:inline-block lg:mt-0 text-white-200 mr-4 hover:text-theme-yellow-dark  cursor-pointer ` +
+                (currentUrl == "info" ? "text-theme-yellow-dark" : "")
+              }
+            >
+              <i className="fa-solid fa-circle-info"></i>
+            </Link>
+            {/* {userInfo?.isAdmin && (
+          <Link
+            to="/adduser"
+            className="text-sm md:text-lg  hover:text-theme-yellow-dark  cursor-pointer"
+          >
+            Add User
+          </Link>
+        )} */}
           </div>
           <div>
-            <button className="inline-flex items-center bg-amber-500 border-0 py-2 px-4 text-white py-0 lg:py-0">
-              Click Me
-            </button>
+            {userInfo ? (
+              <Link
+                className="inline-flex items-center bg-red-500 rounded-lg py-1 px-2 my-3 lg:my-0 text-lg border-0 mx-2 lg:py-0 lg:mx-5"
+                onClick={logOut}
+              >
+                Logout
+              </Link>
+            ) : (
+              <Link
+                to="/login"
+                className="my-auto mx-2  text-theme-yellow-dark active:text-theme-yellow-dark px-2 py-1 rounded-lg  cursor-pointer"
+                // onClick={() => setAuth(true)}
+              >
+                Login
+              </Link>
+            )}
           </div>
         </div>
-      </nav> */}
+      </nav>
     </>
   );
 }
