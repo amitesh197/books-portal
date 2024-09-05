65ec766a98cc16d98089f741d708111cc66dd2b9
amitesh197 <amitesh.sawarkar@gmail.com>
2024-09-05 19:29:35 +0530

    Site updated and live

--- src/api/fetchGoogleSheetData.js
+++ src/api/fetchGoogleSheetData.js
@@ -1,23 +1,25 @@
 import axios from 'axios';
 
 export async function fetchGoogleSheetData() {
-  const API_URL = 'https://sheets.googleapis.com/v4/spreadsheets/1XJlywkzYDy5qk2SU50fyss011DAlFH0TxLrbzGaorfg/values/Sheet1?key=AIzaSyAvFEjU3k501DncNZH7HvgCrnJHHqT8mZ0';
+  const API_URL = 'https://sheets.googleapis.com/v4/spreadsheets/1BWErIXHEh9YDCcPDDsXgulQ-aE_p0NdDtTxrbbH2rC0/values/Sheet1?key=AIzaSyAAe2T-LDq9O-74KILyOMVFHZMzgkUM7Wg';
 
   try {
     const response = await axios.get(API_URL);
-    const rows = response.data.values;
+    const rows = response.data.values || [];
+
+    // Assuming the first row is the header
+    const headers = rows[0] || [];
+    const dataRows = rows.slice(1);
 
     // Format the data into an array of objects
-    const formattedData = rows.map((row, index) => ({
-      name: row[0],
-      mobile: row[1],
-      courseid: row[2],
-      courseName: row[3],
-      couponCode: row[4],
-      TransDate: row[5],
-      amount_paid: row[6],
-      key: index, // Add a key for React rendering
-    }));
+    const formattedData = dataRows.map((row, index) => {
+      let dataObject = {};
+      headers.forEach((header, i) => {
+        dataObject[header] = row[i] || '';
+      });
+      dataObject.key = index; // Add a key for React rendering
+      return dataObject;
+    });
 
     // Reverse the order to show the latest row first
     return formattedData.reverse();
--- src/pages/incentives.jsx
+++ src/pages/incentives.jsx
@@ -2,91 +2,25 @@ import React, { useEffect, useState } from 'react';
 import { fetchGoogleSheetData } from '../api/fetchGoogleSheetData';
 import IncentiveTable from '../components/IncentiveTable';
 import Loading from '../components/Loading';
-import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
-import UserPool from "../UserPool"; // Your UserPool configuration
 import '../stylesheets/query_table/Incentives.scss';
 
 export default function Incentives() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [incentivesData, setIncentivesData] = useState([]);
-  const [selectedAgent, setSelectedAgent] = useState('All');
-  const [selectedMonth, setSelectedMonth] = useState('All');
-  const [agents, setAgents] = useState([]);
-  const [months, setMonths] = useState([]);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [currentAgent, setCurrentAgent] = useState(null);
-  const [passwordInput, setPasswordInput] = useState('');
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // Email to agent name mapping
-  const emailToAgentMapping = {
-    "amiteshs197@gmail.com": "AMIT",
-    "manishabansal788@gmail.com": "Manisha",
-    "rkpandey817@gmail.com": "Ram Pandey",
-    "irfanmindbrink@gmail.com": "Irfan",
-    "sristiverma.rbj@gmail.com": "Srishti Verma",
-    "vermaanchalkumari@gmail.com": "Anchal Verma",
-    "mohitvats01@gmail.com": "Mohit Vats",
-    "harshmishra363@gmail.com": "Harshit Mishra",
-    "sanjaysinghrajawat23#gmail.com": "Sanjay",
-    "shivani.sharma.7194@gmail.com": "Shivani",
-    "apurva000730@gmail.com": "Apurva",
-    "advaitsajal@gmail.com": "Sajal",
-    "sonam.edsarrthi99@gmail.com": "Sonam",
-    "ravikumartanti2@gmail.com": "Ravi",
-    "nikhil2906kumar@gmail.com": "Nikhil",
-    "puru.attrish24@gmail.com": "Purusharth",
-    "iabhaysingh15@gmail.com": "Abhay",
-    "swedhakarnani21@gmail.com": "Swedha Karnani",
-    "shinusuman10@gmail.com": "Sneha",
-    "ankit821022@gmail.com" : "Ankit Singh"
-    // Add other email to agent mappings here
-  };
-
-  // Define agent mapping
-  const agentMapping = {
-    AKR: 'AKR',
-    MBM: 'Manisha',
-    KKS: 'Swedha Karnani',
-    RAM: 'Ram Pandey',
-    IRF: 'Irfan',
-    SRI: 'Srishti Verma',
-    TEZ: 'Sneha',
-    ANC: 'Anchal Verma',
-    MOH: 'Mohit Vats',
-    HAR: 'Harshit Mishra',
-    THI: 'Sanjay',
-    RKS: 'Shivani',
-    APU: 'Apurva',
-    SAR: 'Sajal',
-    KCV: 'Sonam',
-    RKT: 'Ravi',
-    NIK: 'Nikhil',
-    PUR: 'Purusharth',
-    APS: 'Abhay',
-    shivin: 'shivin',
-    oldstudent: 'oldstudent',
-    STUDENTPOWER: 'STUDENTPOWER',
-    MMP: 'MMP',
-    AMI: "AMIT",
-    ANK: "Ankit Singh"
-  };
+  const [userEmail, setUserEmail] = useState('');
 
   useEffect(() => {
+    // Fetch user info from sessionStorage
     const getUserInfo = () => {
       const userInfoString = sessionStorage.getItem('userInfo');
       const userInfo = userInfoString ? JSON.parse(userInfoString) : {};
 
       const email = userInfo.email || 'unknown';
       const isAdmin = userInfo.isAdmin || false;
-      const token = userInfo.token || '';
-
+      setUserEmail(email);
       setIsAdmin(isAdmin);
-      if (!isAdmin) {
-        const agentName = emailToAgentMapping[email];
-        setCurrentAgent(agentName);
-      }
     };
 
     const getData = async () => {
@@ -100,228 +34,62 @@ export default function Incentives() {
       }
     };
 
+    // Process and filter data based on course and user email
     const processData = (fetchedData) => {
-      const incentivesMap = {};
-      const monthsSet = new Set();
+      const filteredData = fetchedData.filter(item => {
+        const courseMatched = item.Course === 'UPSC Prelims Topic-Wise Solved PYQs (2011 - 2024) Book';
+        const emailMatched = item.Email === userEmail;
 
-      fetchedData.forEach((item) => {
-        const couponCode = item.couponCode;
-        const transDate = item.TransDate;
-        const month = new Date(transDate).toLocaleString('default', { month: 'long', year: 'numeric' });
-
-        if (couponCode && typeof couponCode === 'string') {
-          const agentName = agentMapping[couponCode.substring(0, 3)] || couponCode.substring(0, 3);
-          const courseName = item.courseName;
-          const amountPaid = parseFloat(item.amount_paid) || 0;
-
-          if (!incentivesMap[agentName]) {
-            incentivesMap[agentName] = {};
-          }
-
-          if (!incentivesMap[agentName][courseName]) {
-            incentivesMap[agentName][courseName] = {
-              count: 0,
-              totalRevenue: 0,
-              months: new Set()
-            };
-          }
-
-          incentivesMap[agentName][courseName].count += 1;
-          incentivesMap[agentName][courseName].totalRevenue += amountPaid;
-          incentivesMap[agentName][courseName].months.add(month);
-          monthsSet.add(month);
+        if (isAdmin) {
+          return courseMatched;  // Admin sees all rows for the matched course
         }
+        return courseMatched && emailMatched;  // Regular users see only their data
       });
 
-      const formattedData = Object.entries(incentivesMap).flatMap(([agentName, courses]) => {
-        return Object.entries(courses).flatMap(([courseName, { count, totalRevenue, months }]) => {
-          const row = {
-            agentName,
-            courseName,
-            count,
-            totalRevenue
-          };
-
-          return Array.from(months).map(month => ({
-            ...row,
-            month
-          }));
-        });
-      });
-
-      setIncentivesData(formattedData);
-
-      const uniqueAgents = Array.from(new Set(formattedData.map(item => item.agentName)));
-      setAgents(['All', ...uniqueAgents]);
-
-      setMonths(['All', ...Array.from(monthsSet)]);
+      setIncentivesData(filteredData);
     };
 
     getUserInfo();
     getData();
-  }, []);
+  }, [userEmail, isAdmin]);
 
-  const handleAgentChange = (event) => {
-    setSelectedAgent(event.target.value);
-  };
-
-  const handleMonthChange = (event) => {
-    setSelectedMonth(event.target.value);
-  };
-  const handlePasswordSubmit = () => {
-    const userInfoString = sessionStorage.getItem('userInfo');
-    const userInfo = userInfoString ? JSON.parse(userInfoString) : {};
-  
-    const email = userInfo.email || 'unknown';
-    const isAdmin = userInfo.isAdmin || false;
-  
-    if (!email || email === 'unknown') {
-      alert('Email not found in session storage');
-      return;
-    }
-  
-    const authDetails = new AuthenticationDetails({
-      Username: email,
-      Password: passwordInput,
-    });
-  
-    const user = new CognitoUser({
-      Username: email,
-      Pool: UserPool,
-    });
-  
-    user.authenticateUser(authDetails, {
-      onSuccess: async (loginData) => {
-        const accessToken = loginData.getAccessToken().getJwtToken();
-        const accessTokenPayload = loginData.getAccessToken().payload;
-  
-        const newIsAdmin = accessTokenPayload["cognito:groups"]?.includes("admins") || isAdmin;
-  
-        setIsAuthenticated(true);
-        setIsAdmin(newIsAdmin);
-  
-        sessionStorage.setItem(
-          'userInfo',
-          JSON.stringify({
-            username: loginData.accessToken.payload.username,
-            email: loginData.idToken.payload.email,
-            token: accessToken,
-            isAdmin: newIsAdmin,
-          })
-        );
-      },
-      onFailure: (err) => {
-        alert('Incorrect password');
-      },
-    });
-  };
-  
-  const filteredData = incentivesData
-    .filter(item => (isAdmin || (isAuthenticated && item.agentName === currentAgent)) &&
-      (selectedAgent === 'All' || item.agentName === selectedAgent) &&
-      (selectedMonth === 'All' || item.month === selectedMonth)) || [];
-  
-  const totalCourses = filteredData.reduce((acc, item) => acc + item.count, 0);
-  const totalRevenue = filteredData.reduce((acc, item) => acc + item.totalRevenue, 0);
-  
-  const handleRowClick = (row) => {
-    console.log('Row clicked:', row);
-    // Implement navigation or display logic for detailed transactions
-  };
-  
   if (isLoading) {
     return <Loading />;
   }
-  
-  // if (!isAdmin && !isAuthenticated) {
-  //   return (
-  //     <div className="password-container">
-  //       <div className="password-box">
-  //         <h2 className="password-title">Enter Password</h2>
-  //         <input
-  //           type="password"
-  //           value={passwordInput}
-  //           onChange={(e) => setPasswordInput(e.target.value)}
-  //           placeholder="Enter your password"
-  //           className="password-input"
-  //         />
-  //         <button
-  //           onClick={handlePasswordSubmit}
-  //           className="password-button"
-  //         >
-  //           Submit
-  //         </button>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-  
+
   return (
     <div className="container">
-      <h1 className="header">Incentives Summary</h1>
-      
-      <div className="filter-container">
-        {isAdmin && (
-          <div className="filter-group">
-            <label htmlFor="agentDropdown" className="filter-label">Agent Name</label>
-            <select 
-              id="agentDropdown" 
-              value={selectedAgent} 
-              onChange={handleAgentChange} 
-              className="filter-select"
-            >
-              {agents.map(agent => (
-                <option key={agent} value={agent}>{agent}</option>
-              ))}
-            </select>
-          </div>
-        )}
-  
-        <div className="filter-group">
-          <label htmlFor="monthDropdown" className="filter-label">Month</label>
-          <select 
-            id="monthDropdown" 
-            value={selectedMonth} 
-            onChange={handleMonthChange} 
-            className="filter-select"
-          >
-            {months.map(month => (
-              <option key={month} value={month}>{month}</option>
-            ))}
-          </select>
-        </div>
-      </div>
-  
+      <h1 className="header">Books Order Transaction</h1>
+
       <div className="summary">
         <div className="summary-item">
-          <strong>Total Courses: </strong>{totalCourses}
-        </div>
-        <div className="summary-item">
-          <strong>Total Revenue: </strong>{totalRevenue.toLocaleString('en-IN', {
-            style: 'currency',
-            currency: 'INR',
-          })}
+          <strong>Total Records: </strong>{incentivesData.length}
         </div>
       </div>
-  
+
       <IncentiveTable
-        data={filteredData.map(({ agentName, courseName, count, totalRevenue }) => ({
-          agentName,
-          courseName,
-          count,
-          totalRevenue: totalRevenue.toLocaleString('en-IN', {
+        data={incentivesData.map(({ Name, Email, 'Phone Number': PhoneNumber, Course, Date, Amount, 'Coupon Code': CouponCode }) => ({
+          Name,
+          Email,
+          PhoneNumber,
+          Course,
+          Date,
+          Amount: Amount ? parseFloat(Amount).toLocaleString('en-IN', {
             style: 'currency',
             currency: 'INR',
-          }),
+          }) : 'N/A',
+          CouponCode,
         }))}
         columns={[
-          { header: 'Agent Name', accessor: 'agentName' },
-          { header: 'Course Name', accessor: 'courseName' },
-          { header: 'Total Courses', accessor: 'count' },
-          { header: 'Total Revenue', accessor: 'totalRevenue' }
+          { header: 'Name', accessor: 'Name' },
+          { header: 'Email', accessor: 'Email' },
+          { header: 'Phone Number', accessor: 'PhoneNumber' },
+          { header: 'Course', accessor: 'Course' },
+          { header: 'Date', accessor: 'Date' },
+          { header: 'Amount', accessor: 'Amount' },
+          { header: 'Coupon Code', accessor: 'CouponCode' },
         ]}
-        onRowClick={handleRowClick}
       />
     </div>
   );
-  }
+}
